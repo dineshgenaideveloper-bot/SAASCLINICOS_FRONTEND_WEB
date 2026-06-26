442e66dbd64155845c20c2f79931e93d3956e98d
dineshfelicet <dinesh@felicet.com>
2026-06-26 13:53:22 +0530

    fix: repair model exports, finish ESM to CommonJS conversion

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://saasclinicos-backend-8t1dvqumg-dinesh-sass-clinicos-projects.vercel.app/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
